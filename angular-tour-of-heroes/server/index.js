bf4e32966bf6c780c635b9ff53f07a09beb51957
goshima <goshima.k.ab@gmail.com>
2022-10-22 01:27:02 +0900

    test

--- angular-tour-of-heroes/server/index.js
+++ angular-tour-of-heroes/server/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const config = require('./config/dev');
 const FakeDB = require('./fake-db');
 
-const heroesRegisterRoutes = require('./routes/heroes_register'); 
+// const heroesRegisterRoutes = require('./routes/heroes_register'); 
 
 mongoose.connect(config.DB_URI).then(
 	() => {
@@ -14,25 +14,23 @@ mongoose.connect(config.DB_URI).then(
 
 const app = express();
 
-app.use('/api/v1/heroes_register', heroesRegisterRoutes);
+// app.use('/api/v1/heroes_register', heroesRegisterRoutes);
 
-const PORT = process.env.PORT || '3001';
+const PORT = process.env.PORT || '4200';
 
 app.listen(PORT, function() {
 	console.log('I am runnning');
 });
 
-  app.get('/api/heroes', (req, res) => {
+app.get('/api/heroes', (req, res) => {
 	res.send(heroes);
-  })
+})
 
-  app.post('/api/heroes', (req, res) => {
+app.post('/api/heroes', (req, res) => {
 	const hero = {
 		id: heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1: 11,
 		name: "new hero"
 	};
 	heroes.push(hero);
 	res.send(hero);
-  })
-
-  // mongodb+srv://kgoshima:<password>@cluster0.cwo3gda.mongodb.net/?retryWrites=true&w=majority
+})
